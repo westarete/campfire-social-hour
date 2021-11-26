650b5f9f7beab759b549ea3755ad67ef1860a09e
Ryan Schenk <rschenk@gmail.com>
2021-11-26 11:01:32 -0500

    Initial accessibility tweaks

--- lib/campfire_social_hour_web/live/meeting_live/meeting_component.ex
+++ lib/campfire_social_hour_web/live/meeting_live/meeting_component.ex
@@ -3,17 +3,17 @@ defmodule CampfireSocialHourWeb.MeetingLive.MeetingComponent do
 
   def render(assigns) do
     ~H"""
-      <div class="col-md-6 col-sm-6" id={"meeting-#{@id}"}>
+      <div class="col-md-6 col-sm-6" id={"meeting-#{@id}"} aria-label={"#{@topic} room"}>
         <div class="services-box">
           <div class="services-row">
             <div class="services-img">
-              <img src="images/service-img.png" alt="">
+              <img src="images/service-img.png" role="presentation" alt="" />
               <h3>
-                <%= link @topic, to: @link, class: "button button-small", target: "_blank" %>
+                <%= link @topic, to: @link, class: "button button-small", target: "_blank", aria_label: "Join the #{@topic} room" %>
               </h3>
             </div>
             <div class="service-data">
-              <ul id={"participants-#{@id}"}>
+              <ul id={"participants-#{@id}"} aria-label={"Members in the #{@topic} room"} role='list'>
                 <%= for name <- @participants do %>
                   <li><%= name %></li>
                 <% end %>
