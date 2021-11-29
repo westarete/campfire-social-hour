3564e19eacda63a7ee0c0864849190506c87c202
Ryan Schenk <rschenk@gmail.com>
2021-11-29 17:51:07 -0500

    Add production host to prod config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :campfire_social_hour, CampfireSocialHourWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "campfire-social-hour.gigalixirapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
