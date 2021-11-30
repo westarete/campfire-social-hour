ad78e6baae951d1ab6e1936e81dbd6bbf6be8839
Ryan Schenk <rschenk@gmail.com>
2021-11-29 20:17:58 -0500

    force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ import Config
 # before starting your production server.
 config :campfire_social_hour, CampfireSocialHourWeb.Endpoint,
   url: [host: "campfire-social-hour.gigalixirapp.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  force_ssl: [rewrite_on: [:x_forwarded_proto]
 
 # Do not print debug messages in production
 config :logger, level: :info
