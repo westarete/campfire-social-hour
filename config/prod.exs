f6fd218ad207bb621ef22e49bcc7848130db016b
Ryan Schenk <rschenk@gmail.com>
2021-11-29 20:28:22 -0500

    Fixcheck_origin syntax

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :campfire_social_hour, CampfireSocialHourWeb.Endpoint,
   url: [host: "techextra21.westarete.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  check_origin: ["campfire-social-hour.gigalixirapp.com"]
+  check_origin: ["//campfire-social-hour.gigalixirapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
