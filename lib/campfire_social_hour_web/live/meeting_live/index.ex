d1d786f7a7bb964f403c37d302889dda3380e1c4
Ryan Schenk <rschenk@gmail.com>
2021-11-30 11:29:33 -0500

    Always show all people in the zoom rooms

--- lib/campfire_social_hour_web/live/meeting_live/index.ex
+++ lib/campfire_social_hour_web/live/meeting_live/index.ex
@@ -16,32 +16,14 @@ defmodule CampfireSocialHourWeb.MeetingLive.Index do
     {:noreply, update(socket, :meetings, fn _old -> load_meetings() end)}
   end
 
-  def participant_list(participants, limit \\ 8) do
-    truncated_participants =
-      participants
-      |> Enum.reverse()
-      |> Enum.take(limit)
-      |> Enum.map(fn %{"user_name" => user_name} -> user_name end)
-
-    overflow = length(participants) - limit
-
-    do_participant_list(truncated_participants, overflow)
+  def participant_names(participants) do
+    participants
+    |> Enum.reverse()
+    |> Enum.map(fn %{"user_name" => user_name} -> user_name end)
   end
 
   defp load_meetings() do
     ZoomMeetingParticipants.get()
     |> Meetings.list_meetings_with_participants()
-  end
-
-  defp do_participant_list(truncated_names, _overflow = 1) do
-    List.insert_at(truncated_names, -1, "and 1 other")
-  end
-
-  defp do_participant_list(truncated_names, overflow) when 1 < overflow do
-    List.insert_at(truncated_names, -1, "and #{overflow} others")
-  end
-
-  defp do_participant_list(truncated_names, _) do
-    truncated_names
   end
 end
