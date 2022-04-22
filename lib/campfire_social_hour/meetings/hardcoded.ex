da867d50cf8c0adc85451ec7f546e5ff87664fac
Ryan Schenk <rschenk@gmail.com>
2022-04-22 09:29:24 -0400

    Update zoom rooms for Campfire 2022

--- lib/campfire_social_hour/meetings/hardcoded.ex
+++ lib/campfire_social_hour/meetings/hardcoded.ex
@@ -5,33 +5,33 @@ defmodule CampfireSocialHour.Meetings.Hardcoded do
     [
       %Meeting{
         topic: "Ashburn",
-        id: 88_168_094_255,
-        link: "https://us02web.zoom.us/j/88168094255?pwd=SjY0N0VCaTdyRzMwZ1ArajZwemZqUT09"
+        id: 85242087705,
+        link: "https://us02web.zoom.us/j/85242087705?pwd=V1d2Wmc4OU44YmRGWkpLOHMwSW12UT09"
       },
       %Meeting{
         topic: "Chicago",
-        id: 89_509_434_465,
-        link: "https://us02web.zoom.us/j/89509434465?pwd=VnlBazNnemYzWkM2ZnVIeklKOUJ0UT09"
+        id: 82914677712,
+        link: "https://us02web.zoom.us/j/82914677712?pwd=MFJiTytkR2lvQitUS01IY3hLR0pYUT09"
       },
       %Meeting{
         topic: "Dallas",
-        id: 88_241_709_171,
-        link: "https://us02web.zoom.us/j/88241709171?pwd=WEJ1OWZiejdkZlVYMnNSOEVrTXdEQT09"
+        id: 85789329053,
+        link: "https://us02web.zoom.us/j/85789329053?pwd=ZzJBVzFGM2x5VHJEQkpYWnhlUmpPdz09"
       },
       %Meeting{
         topic: "San Jose",
-        id: 81_323_197_405,
-        link: "https://us02web.zoom.us/j/81323197405?pwd=b3hjaE1rMDFGVExaRk5RRzNyOVVmZz09"
+        id: 82271294034,
+        link: "https://us02web.zoom.us/j/82271294034?pwd=UWJUbU9yL3cxT3l3djNQaE5naEJ4QT09"
       },
       %Meeting{
         topic: "NYC",
-        id: 85_141_035_586,
-        link: "https://us02web.zoom.us/j/85141035586?pwd=VEZOejhDaDFpSVdaeGF5TFFDSUdZZz09"
+        id: 88571531135,
+        link: "https://us02web.zoom.us/j/88571531135?pwd=dmM4YlFNWS9TR29XVlFkUkt6b3hCZz09"
       },
       %Meeting{
         topic: "Seattle",
-        id: 83_871_293_505,
-        link: "https://us02web.zoom.us/j/83871293505?pwd=bXBjamxvdGxkMHVVcmVYa1VxY2VtZz09"
+        id: 83008721265,
+        link: "https://us02web.zoom.us/j/83008721265?pwd=MUIwTkU2K1I1ME9rb3BMeGJ2U1F5UT09"
       }
     ]
   end
