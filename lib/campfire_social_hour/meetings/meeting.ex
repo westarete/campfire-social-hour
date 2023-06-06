aaa0d545b060b7bae4fee2c817cee37b3bc6360f
Mark Quezada <mark@mirthlab.com>
2023-06-06 08:28:29 -1000

    Update assets and layout

--- lib/campfire_social_hour/meetings/meeting.ex
+++ lib/campfire_social_hour/meetings/meeting.ex
@@ -1,6 +1,6 @@
 defmodule CampfireSocialHour.Meetings.Meeting do
   @moduledoc false
-  defstruct [:id, :topic, :link]
+  defstruct [:id, :topic, :link, :img_name]
 
   def canonize_id(id), do: to_string(id)
 end
