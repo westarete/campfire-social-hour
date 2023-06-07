4b4415ee1fc7a559b650a8ee03fe92f0749511a0
Mark Quezada <mark@mirthlab.com>
2023-06-06 14:33:35 -1000

    Update tests

--- test/campfire_social_hour/zoom_credential_agent_test.exs
+++ test/campfire_social_hour/zoom_credential_agent_test.exs
@@ -1,16 +1,34 @@
 defmodule CampfireSocialHour.ZoomCredentialAgentTest do
-  use CampfireSocialHour.ZoomApiCase, async: false
+  # use CampfireSocialHour.ZoomApiCase, async: false
+  use ExUnit.Case, async: false
 
   alias CampfireSocialHour.ZoomCredentialAgent
 
+  setup do
+    Mox.stub_with(CampfireSocialHour.ZoomApiMock, CampfireSocialHour.ZoomApiStub)
+    :ok
+  end
+
   describe "access_token/0" do
-    test "when zoom api returns a valid response" do
-      assert ZoomCredentialAgent.access_token() == "abc123"
+    test "when zoom api returns a valid response", %{test: test} do
+      pid = start_supervised!({ZoomCredentialAgent, name: test})
+      Mox.allow(CampfireSocialHour.ZoomApiMock, self(), pid)
+      assert ZoomCredentialAgent.access_token(test) == "abc123"
     end
 
-    test "when zoom api barfs" do
+    test "when zoom api barfs", %{test: test} do
       Mox.stub(CampfireSocialHour.ZoomApiMock, :get_access_token, fn -> :error end)
-      assert ZoomCredentialAgent.access_token() == nil
+
+      Process.flag(:trap_exit, true)
+      pid = start_supervised!({ZoomCredentialAgent, name: test})
+      Mox.allow(CampfireSocialHour.ZoomApiMock, self(), pid)
+
+      error =
+        catch_exit do
+          ZoomCredentialAgent.access_token(test)
+        end
+
+      assert {{{:case_clause, :error}, _}, _} = error
     end
   end
 end
