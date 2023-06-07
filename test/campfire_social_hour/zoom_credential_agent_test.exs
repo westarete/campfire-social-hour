137f610bc6bb9dc091a4e5f374529ff0177de65d
Mark Quezada <mark@mirthlab.com>
2023-06-07 12:48:00 -1000

    Cleanup and use stub

--- test/campfire_social_hour/zoom_credential_agent_test.exs
+++ test/campfire_social_hour/zoom_credential_agent_test.exs
@@ -6,22 +6,18 @@ defmodule CampfireSocialHour.ZoomCredentialAgentTest do
 
   setup do
     put_env(:campfire_social_hour, :zoom_api, CampfireSocialHour.ZoomApiMock)
-    # Mox.stub_with(CampfireSocialHour.ZoomApiMock, CampfireSocialHour.ZoomApiStub)
+    Mox.stub_with(CampfireSocialHour.ZoomApiMock, CampfireSocialHour.ZoomApiStub)
     :ok
   end
 
   describe "access_token/0" do
     test "when zoom api returns a valid response", %{test: test} do
-      Mox.stub(CampfireSocialHour.ZoomApiMock, :get_access_token, fn ->
-        {:ok, %{access_token: "fauxtoken", expires_in: 5 * 60}}
-      end)
-
       pid =
         start_supervised!({ZoomCredentialAgent, name: test, initial_state: %{access_token: nil}})
 
       Mox.allow(CampfireSocialHour.ZoomApiMock, self(), pid)
 
-      assert ZoomCredentialAgent.access_token(pid) == "fauxtoken"
+      assert ZoomCredentialAgent.access_token(pid) == "abc123"
     end
 
     test "when zoom api barfs", %{test: test} do
