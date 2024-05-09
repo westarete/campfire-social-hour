1f5a5594298210d6997b3ed40300a580071f919b
Ryan Schenk <rschenk@gmail.com>
2024-05-09 16:02:15 -0400

    Finalize zoom links for Basecamp 2024

--- lib/campfire_social_hour/meetings/hardcoded.ex
+++ lib/campfire_social_hour/meetings/hardcoded.ex
@@ -18,38 +18,38 @@ defmodule CampfireSocialHour.Meetings.Hardcoded do
       },
       %Meeting{
         topic: "Ghost Stories",
-        id: 82_951_060_700,
-        link: "https://westarete.zoom.us/j/82951060700?pwd=a2ZQUWNHbGpTejY0K1NmNzN1NjZ3Zz09",
+        id: 81_469_598_503,
+        link: "https://westarete.zoom.us/j/81469598503?pwd=M3Y1M1dXUHFkdHRhOENnQStLSHNiUT09",
         img_name: "CAMPFire_GhostStory.png"
       },
       %Meeting{
         topic: "Camp Fire Clairvoyance",
-        id: 81_469_598_503,
-        link: "https://westarete.zoom.us/j/81469598503?pwd=M3Y1M1dXUHFkdHRhOENnQStLSHNiUT09",
+        id: 82_951_060_700,
+        link: "https://westarete.zoom.us/j/82951060700?pwd=a2ZQUWNHbGpTejY0K1NmNzN1NjZ3Zz09",
         img_name: "CAMPFire_CrystalBall.png"
       },
       %Meeting{
         topic: "Let's Talk S'More - 1",
-        id: 82_924_055_582,
-        link: "https://westarete.zoom.us/j/82924055582?pwd=VExYbHlGMDB4WXl0cG1YQy81dmRzZz09",
+        id: 85_122_377_210,
+        link: "https://westarete.zoom.us/j/85122377210?pwd=RTA5TWk1aUZHWkt5TG9PYlRGM1NwQT09",
         img_name: "CAMPFire_Smore.png"
       },
       %Meeting{
         topic: "Let's Talk S'More - 2",
+        id: 86_768_169_687,
+        link: "https://westarete.zoom.us/j/86768169687?pwd=SGNqSk5RR3ZBTG1nY2o2U1NuUXNLdz09",
+        img_name: "CAMPFire_Smore.png"
+      },
+      %Meeting{
+        topic: "Let's Talk S'More - 3",
         id: 82_016_201_416,
         link: "https://westarete.zoom.us/j/82016201416?pwd=YkVCbmROQk9UMndzc2tWNm9oQ2JMQT09",
         img_name: "CAMPFire_Smore.png"
       },
       %Meeting{
-        topic: "Let's Talk S'More - 3",
-        id: 89_185_203_778,
-        link: "https://westarete.zoom.us/j/89185203778?pwd=L1MzWVhKaFhuQ0h1eDkyakRlTlQ5UT09",
-        img_name: "CAMPFire_Smore.png"
-      },
-      %Meeting{
         topic: "Let's Talk S'More - 4",
-        id: 89_185_203_779,
-        link: "https://westarete.zoom.us/j/89185203779?pwd=L1MzWVhKaFhuQ0h1eDkyakRlTlQ5UT09",
+        id: 82_924_055_582,
+        link: "https://westarete.zoom.us/j/82924055582?pwd=VExYbHlGMDB4WXl0cG1YQy81dmRzZz09",
         img_name: "CAMPFire_Smore.png"
       }
     ]
